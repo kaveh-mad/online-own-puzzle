c6150f8498d6fc1851e223ba16c5f37fd0cbae12
kaveh-mad@ <mad.kaveh@gmail.com>
2019-01-07 12:45:22 +0330

    free onlion pleing puzzle

---
+++ offline.js
@@ -0,0 +1,27 @@
+﻿{
+	"version": 1546849151,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/spritefont.png",
+		"images/sprite-sheet0.png",
+		"images/fram-sheet0.png",
+		"images/loder-sheet0.png",
+		"images/item3-sheet0.png",
+		"images/item2-sheet0.png",
+		"images/item1-sheet0.png",
+		"images/spritefont2.png",
+		"images/spritefont3.png",
+		"images/spritefont4.png",
+		"media/clic-clac-mp4.m4a",
+		"media/clic-clac-mp4.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
